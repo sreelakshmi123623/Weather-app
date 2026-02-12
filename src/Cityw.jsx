b468620150ed15e868eaeb7679a41c0a8bb80395
sreelakshmi123623 <sreel5148@gmail.com.com>
2026-02-12 21:00:44 +0530

    edited

--- src/Cityw.jsx
+++ src/Cityw.jsx
@@ -18,7 +18,7 @@ function Cityw() {
  .then((data)=>{
   if(data.cod==="404"){
     setError("city Not Found")
-    alert("city Not Found")
+    alert("City Not Found")
     setWeather(null)
   }
 
@@ -37,7 +37,7 @@ function Cityw() {
       <div className='h-150 w-120 rounded-lg bg-blue-900 border-1 border-white ml-130'>
         <h2 className='text-center text-2xl text-white pt-3'>Weather Check</h2>
         <div>
-          <input type="text" placeholder='Enter city' value={city} onChange={(e)=>setCity(e.target.value)} onKeyDown={(e)=>e.key==="Enter" && getWeather()} className='w-90px mt-10 ml-35 p-1 bg-white rounded-md border-1' />
+          <input type="text" placeholder=' Enter city name' value={city} onChange={(e)=>setCity(e.target.value)} onKeyDown={(e)=>e.key==="Enter" && getWeather()} className='w-90px mt-10 ml-25 p-1 bg-white rounded-md border-1' />
           <button variant="contained" onClick={getWeather} className=' text-white rounded-md p-1 ml-3 hover:bg-white hover:text-black border-1'>Search</button>
           <img className='w-50 h-50 ml-35' src="https://attic.sh/q7tw7mp0sf5cdtrvog8e9ochmjpg" alt="" />
          {weather && (
